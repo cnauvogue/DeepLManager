fb9eb4464cd1ce3da10dee65bfad94445d01ef3b
Adam K. Schmidt <cnauvogue@proton.me>
2024-01-25 16:22:12 +0000

    Set terms as root

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   resources :terms
   resources :languages
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
   resources :translations
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
@@ -10,5 +8,5 @@ Rails.application.routes.draw do
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
-  root "translations#index"
+  root "terms#index"
 end
