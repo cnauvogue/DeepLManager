6904317fa1da58f4909fa5b931fa773dc7b3df90
Adam K. Schmidt <cnauvogue@proton.me>
2024-02-02 13:12:59 +0000

    Remove translations index

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,11 @@
 Rails.application.routes.draw do
-  resources :terms do 
-    collection do 
+  resources :terms do
+    collection do
       get :submit
     end
   end
   resources :languages
-  resources :translations
+  resources :translations, except: [:index]
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
